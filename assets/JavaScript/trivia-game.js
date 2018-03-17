7f641a31b11e1e8c1b2184c53d7ba8eee9332ff0
Anthony Charles Scott Jr <acsii@me.com>
2018-03-16 22:03:04 -0400

    Trivia-Game v2 Design Complete

--- assets/JavaScript/trivia-game.js
+++ assets/JavaScript/trivia-game.js
@@ -1,31 +1,33 @@
-
-
-
 var eDrive = [
     {
         question: "In what year did BMW start selling the i3 and i8 models?",
         answer: "2014",
-        choices: ["2013", "2014", "2015", "2016"]
+        choices: ["2013", "2014", "2015", "2016"],
+        photo: "assets/Images/bmw1.gif"
     },
     {
         question: "How long does it take for the 2018 BMW i3 (without Range Extender) to accelerate from 0-60mph?",
         answer: "7.2 seconds",
-        choices: ["8.3 seconds","8.9 seconds","6.5 seconds","7.2 seconds" ]
+        choices: ["8.3 seconds","8.9 seconds","6.5 seconds","7.2 seconds" ],
+        photo: "assets/Images/bmw2.gif"
     },
     {
         question: "Which of the following is not a 2018 BMW i8 design package?",
         answer: "Deka World",
-        choices: ["Deka World", "Mega World", "Giga World", "Tera World"]
+        choices: ["Deka World", "Mega World", "Giga World", "Tera World"],
+        photo: "assets/Images/bmw3.gif"
     },
     {
         question: "How far can the 2018 BMW i3 (without Range Extender) travel on a single charge?",
         answer: "up to 114 miles",
-        choices: ["up to 88 miles", "up to 194 miles", "up to 100 miles", "up to 114 miles"]
+        choices: ["up to 88 miles", "up to 194 miles", "up to 100 miles", "up to 114 miles"],
+        photo: "assets/Images/bmw4.gif"
     },
     {
-        question: "How long does it take for the 2018 BMW i8 Coupe to accelerate from 0-60mph?",
+        question: "How long does it take for the 2018 BMW i8 to accelerate from 0-60mph?",
         answer: "4.2 seconds",
-        choices: ["6 seconds", "3.8 seconds", "5.5 seconds", "4.2 seconds"]
+        choices: ["6 seconds", "3.8 seconds", "5.5 seconds", "4.2 seconds"],
+        photo: "assets/Images/bmw5.gif"
     }
 ];
 
@@ -36,10 +38,7 @@ var unAns = 0;
 var intervalId;
 var countDown = 20;
 var eCount = 0;
-
-
-
-console.log(eDrive[eIndex].question);
+var image1 = new Image();
 
 $(document).ready(function () {
 
@@ -50,14 +49,14 @@ $(document).ready(function () {
         $("#choice3").append(eDrive[eIndex].choices[2]);
         $("#choice4").append(eDrive[eIndex].choices[3]);
         $("#startButton").empty();
-
-
         start();
-
-
     });
 
     $("body").on("click", "#restartButton", function () {
+        $("#gameMSG").empty();
+        $("#rightAns").empty();
+        $("#wrongAns").empty();
+        $("#notAns").empty();
         eNext();
     });
 
@@ -65,110 +64,124 @@ $(document).ready(function () {
 
         if (eDrive[eIndex].choices[0] === eDrive[eIndex].answer)
         {
+            image1.src = eDrive[eIndex].photo;
             $("#quesEtc").text("Correct!");
             $("#choice1").empty();
             $("#choice2").empty();
             $("#choice3").empty();
             $("#choice4").empty();
+            $("#imgBMW").append(image1);
             stop();
             corAns++;
             eIndex++;
             eCount++;
-            setTimeout(eNext,3000);
+            setTimeout(eNext,5000);
         } else {
+            image1.src = eDrive[eIndex].photo;
             $("#quesEtc").text("Nope!");
             $("#choice1").text("The correct answer was: " +eDrive[eIndex].answer);
             $("#choice2").empty();
             $("#choice3").empty();
             $("#choice4").empty();
+            $("#imgBMW").prepend(image1);
             stop();
             incAns++;
             eIndex++;
             eCount++;
-            setTimeout(eNext,3000);
+            setTimeout(eNext,5000);
         }
 
     });
     $("body").on("click", "#choice2", function () {
         if (eDrive[eIndex].choices[1] === eDrive[eIndex].answer)
         {
+            image1.src = eDrive[eIndex].photo;
             $("#quesEtc").text("Correct!");
             $("#choice1").empty();
             $("#choice2").empty();
             $("#choice3").empty();
             $("#choice4").empty();
+            $("#imgBMW").append(image1);
             stop();
             corAns++;
             eIndex++;
             eCount++;
-            setTimeout(eNext,3000);
+            setTimeout(eNext,5000);
         } else {
+            image1.src = eDrive[eIndex].photo;
             $("#quesEtc").text("Nope!");
             $("#choice1").text("The correct answer was: " +eDrive[eIndex].answer);
             $("#choice2").empty();
             $("#choice3").empty();
             $("#choice4").empty();
+            $("#imgBMW").prepend(image1);
             stop();
             incAns++;
             eIndex++;
             eCount++;
-            setTimeout(eNext,3000);
-
+            setTimeout(eNext,5000);
         }
     });
     $("body").on("click", "#choice3", function () {
         if (eDrive[eIndex].choices[2] === eDrive[eIndex].answer)
         {
+            image1.src = eDrive[eIndex].photo;
             $("#quesEtc").text("Correct!");
             $("#choice1").empty();
             $("#choice2").empty();
             $("#choice3").empty();
             $("#choice4").empty();
+            $("#imgBMW").append(image1);
             stop();
             corAns++;
             eIndex++;
             eCount++;
-            setTimeout(eNext,3000);
+            setTimeout(eNext,5000);
         } else {
+            image1.src = eDrive[eIndex].photo;
             $("#quesEtc").text("Nope!");
             $("#choice1").text("The correct answer was: " +eDrive[eIndex].answer);
             $("#choice2").empty();
             $("#choice3").empty();
             $("#choice4").empty();
+            $("#imgBMW").prepend(image1);
             stop();
             incAns++;
             eIndex++;
             eCount++;
-            setTimeout(eNext,3000);
+            setTimeout(eNext,5000);
         }
     });
     $("body").on("click", "#choice4", function () {
         if (eDrive[eIndex].choices[3] === eDrive[eIndex].answer)
         {
+            image1.src = eDrive[eIndex].photo;
             $("#quesEtc").text("Correct!");
             $("#choice1").empty();
             $("#choice2").empty();
             $("#choice3").empty();
             $("#choice4").empty();
+            $("#imgBMW").append(image1);
             stop();
             corAns++;
             eIndex++;
             eCount++;
-            setTimeout(eNext,3000);
+            setTimeout(eNext,5000);
         } else {
+            image1.src = eDrive[eIndex].photo;
             $("#quesEtc").text("Nope!");
             $("#choice1").text("The correct answer was: " +eDrive[eIndex].answer);
             $("#choice2").empty();
             $("#choice3").empty();
             $("#choice4").empty();
+            $("#imgBMW").prepend(image1);
             stop();
             incAns++;
             eIndex++;
             eCount++;
-            setTimeout(eNext,3000);
+            setTimeout(eNext,5000);
         }
     });
-
 
 });
 
@@ -187,16 +200,18 @@ function count() {
     var converted = timeConverter(countDown);
     $("#time-rem").text(converted);
     if (countDown === 0) {
+        image1.src = eDrive[eIndex].photo;
         $("#quesEtc").text("Out of Time!");
         $("#choice1").text("The correct answer was: " +eDrive[eIndex].answer);
         $("#choice2").empty();
         $("#choice3").empty();
         $("#choice4").empty();
+        $("#imgBMW").prepend(image1);
         stop();
         unAns++;
         eIndex++;
         eCount++;
-        setTimeout(eNext,3000);
+        setTimeout(eNext,5000);
     }
 }
 
@@ -216,19 +231,24 @@ function eNext () {
         $("#choice3").text(eDrive[eIndex].choices[2]);
         $("#choice4").text(eDrive[eIndex].choices[3]);
         $("#restartButton").empty();
+        $("#imgBMW").empty();
         countDown = 20;
 
         start();
     } else {
-        $("#quesEtc").text("All done, here's how you did!");
-        $("#choice1").text("Correct Answers: " +corAns);
-        $("#choice2").append("Incorrect Answers: " +incAns);
-        $("#choice3").append("Unanswered: " +unAns);
-        setTimeout(eRestart,3000);
+        $("#choice1").empty();
+        $("#quesEtc").empty();
+        $("#gameMSG").text("All done, here's how you did!");
+        $("#rightAns").text("Correct Answers: " +corAns);
+        $("#wrongAns").append("Incorrect Answers: " +incAns);
+        $("#notAns").append("Unanswered: " +unAns);
+        setTimeout(eRestart,5000);
+        $("#imgBMW").empty();
     }
 
 function eRestart () {
     $("#restartButton").append("Start Over?");
+    $("#quesEtc").empty();
     eIndex = 0;
     unAns = 0;
     incAns = 0;
@@ -237,7 +257,3 @@ function eRestart () {
     }
 
 }
-
-
-
-
